04954f8cfdd8d03c82e11315e6152e6680dccaaf
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2020-04-04 20:47:29 -0300

    Add TaxRateConsole

---
+++ TaxRateConsole/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using TaxRateConsole.Entities;
+
+namespace TaxRateConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TaxPayer> payers = new List<TaxPayer>();
+
+            Console.Write("Enter the number of tax payers: ");
+            double n = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data: ");
+                Console.Write("Individual or company (i/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'i' || ch == 'I')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    payers.Add(new Individual(name, anualIncome, healthExpenditures));
+                }
+                else
+                {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    payers.Add(new Company(name, anualIncome, numberOfEmployees));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TAXES PAID: ");
+            double total = 0.0;
+            foreach (TaxPayer payer in payers)
+            {
+                total += payer.Tax();
+                Console.WriteLine($"{payer.Name}: $ {payer.Tax().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("TOTAL TAXES: $ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
